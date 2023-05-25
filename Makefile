8eaa2af453ca1b20ced4e82bdc73519de6cdc5a1
Rhea Myers <rhea@myers.studio>
2023-05-25 15:02:58 -0700

    Clean up and add colour.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-JS = point.js turtle.js polyline.js drawing.js drawing_canvas.js
+JS = MersenneTwister.js point.js turtle.js polyline.js palette.js drawing.js drawing_canvas.js
 
 draw_something_canvas.js: $(JS)
 	browserify -r ./drawing_canvas.js:drawing_canvas --outfile ${@}
--- drawing.js
+++ drawing.js
@@ -19,8 +19,10 @@
 
 // Keep these names lowercase as they should be in the object...
 
-var Polyline = require('./polyline');
-var Turtle = require('./turtle');
+var Polyline = require("./polyline");
+var Turtle = require("./turtle");
+var Palette = require("./palette");
+var MersenneTwister = require("./MersenneTwister");
 
 var max_points_guard = 5000;
 
@@ -28,76 +30,82 @@ var pen_distance = 5.0;
 var pen_distance_fuzz = 1.5; 
 var pen_forward_step = 2.0;
 var pen_turn_step = 1.0;
+// This will have 1 added to it.
+var max_outline_width = 7.0;
 
-var Drawing = function (width , height , num_points) {
+var Drawing = function (width , height , num_points, randseed) {
+  this.randseed = randseed;
+  this.rng = new MersenneTwister(this.randseed);
   this.width = width;
   this.height = height;
   this.skeleton = this.make_skeleton (this.width, this.height, num_points);
   this.pen = this.make_pen ();
   this.first_point = this.pen.position;
   this.outline = new Polyline();
-}
+  this.palette = new Palette(this.rng);
+  this.outline_width = 1 + Math.ceil(this.rng.random() * max_outline_width);
+};
 
 Drawing.prototype.make_skeleton = function (width, height, num_points) {
   // Inset skeleton 2 * the pen distance from the edge to avoid cropping
   var inset = pen_distance * 2;
-  skeleton = new Polyline ();
-  skeleton.random_points_in_bounds (inset, inset, width - (inset * 2),
-					                height - (inset * 2), num_points);
+  var skeleton = new Polyline ();
+  skeleton.random_points_in_bounds (this.rng, inset, inset, width - (inset * 2),
+    height - (inset * 2), num_points);
   return skeleton;
-}
+};
 
 Drawing.prototype.make_pen = function () {
   var top_left = this.skeleton.top_leftmost_point ();
   top_left.y -= pen_distance;
-  pen = new Turtle (top_left, pen_distance, pen_distance_fuzz,
-			        pen_forward_step, pen_turn_step);
+  var pen = new Turtle (top_left, pen_distance, pen_distance_fuzz,
+    pen_forward_step, pen_turn_step);
   return pen;
-}
+};
 
 Drawing.prototype.should_finish = function () {
   var point_count = this.outline.points.length;
-  var first_point = this.outline.points[0]
+  var first_point = this.outline.points[0];
   return (point_count > max_points_guard) || 
-	    ((point_count > 4)
+      ((point_count > 4)
          && (this.pen.position.distance_to_point (first_point)
              < this.pen.forward_step));
-}
+};
 
 Drawing.prototype.next_pen_distance = function ()
 {
   return this.skeleton.distance_to_point (this.pen.next_point_would_be ());
-}
+};
 
 Drawing.prototype.next_pen_too_far = function ()
 {
-  return (Math.random () * pen_distance_fuzz) <
-	(pen_distance - this.next_pen_distance (this.skeleton));
-}
+  return (this.rng.random () * pen_distance_fuzz) <
+  (pen_distance - this.next_pen_distance (this.skeleton));
+};
 
 Drawing.prototype.next_pen_too_close = function ()
 {
-	return (Math.random () * pen_distance_fuzz) <
-	(this.next_pen_distance (this.skeleton) - pen_distance);
-}
+  return (this.rng.random () * pen_distance_fuzz) <
+  (this.next_pen_distance (this.skeleton) - pen_distance);
+};
 
 Drawing.prototype.ensure_next_pen_far_enough = function ()
 {
   while (this.next_pen_too_close (this.skeleton))
-	this.pen.left ();
-}
+    this.pen.left ();
+};
 
 Drawing.prototype.ensure_next_pen_close_enough = function ()
 {
   while (this.next_pen_too_far (this.skeleton))
-	this.pen.right ();
-}
+    this.pen.right ();
+};
 
 Drawing.prototype.adjust_next_pen = function ()
 {
   this.ensure_next_pen_far_enough ();
   this.ensure_next_pen_close_enough ();
-}
+};
 
 Drawing.prototype.to_next_point = function ()
 {
@@ -106,6 +114,6 @@ Drawing.prototype.to_next_point = function ()
   this.outline.append(this.pen.position);
   this.point_count ++;
   return this.pen.position;
-}
+};
 
 module.exports = Drawing;
